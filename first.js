7bbb8ce0a9f7137cb36a0f1a73bf95b63d3f9360
Manthan <manthanjswl@gmail.com>
2026-06-30 16:57:34 +0530

    initial commit

---
+++ first.js
@@ -0,0 +1,17 @@
+console.log("Hello World !");
+console.error("This is an error message !");
+console.warn("This is a warning !");
+var name, age;
+name = "Dogge";
+age = 19;
+var year = 2026;
+console.log(name, age, year);
+const { random } = require('./module');
+console.log(random);
+//Objets
+const Name = 'Username';
+const obj = {
+    [Name]: 'Dogge',
+    Age: 19
+}
+console.log(obj);
