acbfda2eb67b80d84268dac2675d685cca7e808d
Manthan <manthanjswl@gmail.com>
2026-08-03 15:30:09 +0530

    Tailwind

---
+++ Tailwind/src/components/section1/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className='flex justify-between items-center py-6 px-16'>
+            <h4 className='bg-black text-white px-6 uppercase py-2 rounded-full text-sm'>Target Audience</h4>
+            <button className='bg-black text-white py-2 px-6 uppercase rounded-full text-sm'>Digtal Banking Platform</button>
+        </div>
+    )
+}
+ 
+export default Navbar
