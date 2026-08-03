e2ad0d9ab5a322ace191e45808ad3bd019bba486
Manthan <manthanjswl@gmail.com>
2026-08-03 15:29:44 +0530

    Tailwind

---
+++ Tailwind/src/components/section1/HeroText.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HeroText = () => {
+    return (
+
+        <div className='p-5'>
+            <h3 className='text-6xl font-bold mb-7'>Prospective <br />Lauda Lasan</h3>
+            <p className='text-xl text-gray-800 font-medium '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et quibusdam porro quia veniam, distinctio, in nulla repellendus voluptatum ad vitae perferendis dicta nostrum, asperiores sed magnam. Suscipit sed in sunt?</p>
+        </div>
+
+    )
+}
+
+export default HeroText
